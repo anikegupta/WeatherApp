80d13e3f61047ac234059ac41ae98fe4669e7171
aniketgupta <aniketgupta36625@gmail.com>
2025-08-24 15:51:27 +0530

    changes

--- src/services/WeatherServices.js
+++ src/services/WeatherServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiKey="c305a6e2c0f940b6b00152913252008"
-const baseUrl="http://api.weatherapi.com/v1/"
+const baseUrl="https://api.weatherapi.com/v1/"
 
 export const getWeather=async (city)=>{
     const completeUrl=`${baseUrl}/current.json?key=${apiKey}&q=${city}`
